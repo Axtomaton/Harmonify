08dedcc625e51a101d92ff536ac47906b36c766c
Ivan Lin <69885102+Axtomaton@users.noreply.github.com>
2024-01-30 13:37:02 -0500

    Class activity 1/30

--- Core/Src/Homework/printf.c
+++ Core/Src/Homework/printf.c
@@ -11,16 +11,16 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-char buffer[256]; //buffer to store
+char buffer[256]; //buffer to store formatted string
 
 int do_printf(const char* format, ...){
-	va_list aptr;
-	int ret;
+	va_list aptr; //traverse arguments
+	int ret; //return value of vsprintf
 	va_start (aptr, format);
-	ret = vsprintf(buffer, format, aptr);
+	ret = vsprintf(buffer, format, aptr); //formats the string
 	va_end (aptr);
-	USART_Write(USART2, (unsigned char*)buffer, ret);
-	return (ret);
+	USART_Write(USART2, (unsigned char*)buffer, ret); //writes it to minicom
+	return (ret); //number of chars written to the buffer.
 }
 
 
