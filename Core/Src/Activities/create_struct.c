e20283bb8082422909b5f573f04e5dce437f9258
Ivan Lin <69885102+Axtomaton@users.noreply.github.com>
2024-02-05 21:34:04 -0500

    hw2 submission

--- Core/Src/Activities/create_struct.c
+++ Core/Src/Activities/create_struct.c
@@ -6,3 +6,14 @@
  */
 
 
+
+//create a struct for the MIDI header
+
+struct MIDI_Header{
+	char ChunkType [4];
+	unsigned int headerLength; //32 bit length
+	unsigned short format; //16 bits
+	unsigned short ntrks; //16 bits
+	unsigned short division; //bits
+
+};
