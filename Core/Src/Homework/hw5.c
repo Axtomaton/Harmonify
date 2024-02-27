f595da7388f609873214757444383a0fe6a8e55d
Ivan Lin <69885102+Axtomaton@users.noreply.github.com>
2024-02-27 09:53:06 -0500

    Added additional comments for hw5

--- Core/Src/Homework/hw5.c
+++ Core/Src/Homework/hw5.c
@@ -1,3 +1,10 @@
+/**
+ * @file hw5.c
+ * @author Ivan Lin -il9082@rit.edu
+ * @brief File for homework 5
+ * @date 2024-02-27
+ */
+
 
 #include "hw5.h"
 #include "LED.h"
@@ -6,6 +13,11 @@
 #include "printf.h"
 #include "Activity_8.h"
 
+
+/**
+ * @brief This function runs the test for the LED and UART. 
+ * It toggles the LED and echos the user input through USART_Read_Nonblocking
+ */
 void test_LED(){
     init_systick(); // Initialize SysTick for delay
     while(1) {
